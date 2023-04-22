770d80bbc2efaaa0067a47b92daca76733f03300
lallamilagro <for.lenochka@gmail.com>
2023-04-22 22:55:28 +0300

    Power

---
+++ 8 kyu/Power.js
@@ -0,0 +1,8 @@
+/* The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times). */
+
+
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    return number**=power;  
+  }
+  
